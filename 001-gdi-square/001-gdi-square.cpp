65bca77de42b67bb196d55a7dfda73af5b93bb7c
a.vinogradov <a.vinogradov@bpcbt.com>
2024-10-04 16:37:14 +0300

    upd

--- 001-gdi-square/001-gdi-square.cpp
+++ 001-gdi-square/001-gdi-square.cpp
@@ -4,14 +4,14 @@
 #include "001-gdi-square.h"
 #include "framework.h"
 
-constexpr auto MAX_LOADSTRING = 100;
+constexpr auto MAX_LOAD_STRING = 100;
 
 // Global Variables:
 HINSTANCE hInst; // current instance
-WCHAR szTitle[MAX_LOADSTRING]; // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING]; // the main window class name
+WCHAR szTitle[MAX_LOAD_STRING]; // The title bar text
+WCHAR szWindowClass[MAX_LOAD_STRING]; // the main window class name
 
-HBRUSH hAppBakgroundBrush = CreateSolidBrush(RGB(229, 232, 235));
+HBRUSH hAppBackgroundBrush = CreateSolidBrush(RGB(229, 232, 235));
 HBRUSH hSquareFillBrush = CreateSolidBrush(RGB(138, 187, 255));
 HPEN hSquareBorderPen = CreatePen(PS_SOLID, 4, RGB(45, 114, 210));
 
@@ -21,18 +21,19 @@ BOOL InitInstance(HINSTANCE, int);
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                      _In_opt_ HINSTANCE hPrevInstance,
-                      _In_ LPWSTR lpCmdLine,
-                      _In_ int nCmdShow) {
+int APIENTRY wWinMain(
+    _In_ const HINSTANCE hInstance,
+    _In_opt_ const HINSTANCE hPrevInstance,
+    _In_ const LPWSTR lpCmdLine,
+    _In_ const int nCmdShow) {
   UNREFERENCED_PARAMETER(hPrevInstance);
   UNREFERENCED_PARAMETER(lpCmdLine);
 
   // TODO: Place code here.
 
   // Initialize global strings
-  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-  LoadStringW(hInstance, IDC_MY001GDISQUARE, szWindowClass, MAX_LOADSTRING);
+  LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOAD_STRING);
+  LoadStringW(hInstance, IDC_MY001GDISQUARE, szWindowClass, MAX_LOAD_STRING);
   MyRegisterClass(hInstance);
 
   // Perform application initialization:
@@ -40,8 +41,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     return FALSE;
   }
 
-  HACCEL hAccelTable = LoadAccelerators(hInstance,
-                                        MAKEINTRESOURCE(IDC_MY001GDISQUARE));
+  HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY001GDISQUARE));
 
   MSG msg;
 
@@ -56,7 +56,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
   return static_cast<int>(msg.wParam);
 }
 
-ATOM MyRegisterClass(HINSTANCE hInstance) {
+ATOM MyRegisterClass(const HINSTANCE hInstance) {
   WNDCLASSEXW wcex;
 
   wcex.cbSize = sizeof(WNDCLASSEX);
@@ -72,25 +72,26 @@ ATOM MyRegisterClass(HINSTANCE hInstance) {
   wcex.lpszClassName = szWindowClass;
   wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
   // wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-  wcex.hbrBackground = hAppBakgroundBrush;
+  wcex.hbrBackground = hAppBackgroundBrush;
 
   return RegisterClassExW(&wcex);
 }
 
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
+BOOL InitInstance(const HINSTANCE hInstance, const int nCmdShow) {
   hInst = hInstance; // Store instance handle in our global variable
 
-  HWND hWnd = CreateWindowW(szWindowClass,
-                            szTitle,
-                            WS_OVERLAPPEDWINDOW,
-                            CW_USEDEFAULT,
-                            0,
-                            1000,
-                            1000,
-                            nullptr,
-                            nullptr,
-                            hInstance,
-                            nullptr);
+  HWND hWnd = CreateWindowW(
+      szWindowClass,
+      szTitle,
+      WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT,
+      0,
+      1000,
+      1000,
+      nullptr,
+      nullptr,
+      hInstance,
+      nullptr);
 
   if (!hWnd) {
     return FALSE;
@@ -102,7 +103,7 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow) {
   return TRUE;
 }
 
-void invalidateDrawArea(const HWND hWnd) {
+void InvalidateDrawArea(const HWND hWnd) {
   RECT rect;
   GetClientRect(hWnd, &rect);
 
@@ -111,16 +112,15 @@ void invalidateDrawArea(const HWND hWnd) {
   InvalidateRect(hWnd, &rect, TRUE);
 }
 
-LRESULT CALLBACK
-WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK WndProc(const HWND hWnd, const UINT message, const WPARAM wParam, const LPARAM lParam) {
   static int sx, sy;
   static auto sizeFactor = 1.;
   static auto isAutoResizing = FALSE;
   static auto autoResizingDirection = +1;
 
   switch (message) {
-    case WM_CREATE: {
-      CreateWindowEx(
+  case WM_CREATE: {
+    CreateWindowEx(
         0,
         L"BUTTON",
         L"Start",
@@ -133,133 +133,137 @@ WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
         (HMENU)IDC_TOGGLE_BUTTON,
         (HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
         nullptr);
+    break;
+  }
+
+  case WM_COMMAND: {
+    const int wmId = LOWORD(wParam);
+    // Parse the menu selections:
+    switch (wmId) {
+    case IDM_ABOUT: {
+      DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
       break;
     }
 
-    case WM_COMMAND: {
-      const int wmId = LOWORD(wParam);
-      // Parse the menu selections:
-      switch (wmId) {
-        case IDM_ABOUT: {
-          DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-          break;
-        }
-
-        case IDM_EXIT: {
-          DestroyWindow(hWnd);
-          break;
-        }
-
-        case IDC_TOGGLE_BUTTON: {
-          const wchar_t *newLabel;
-          KillTimer(hWnd, IDT_AUTO_RESIZE_TIMER);
-          if (isAutoResizing) {
-            newLabel = L"Start";
-            isAutoResizing = FALSE;
-          } else {
-            newLabel = L"Stop";
-            SetTimer(hWnd, IDT_AUTO_RESIZE_TIMER, 100, nullptr);
-            isAutoResizing = TRUE;
-          }
-          SetWindowTextW(GetDlgItem(hWnd, IDC_TOGGLE_BUTTON), newLabel);
-          break;
-        }
-
-        default:
-          return DefWindowProc(hWnd, message, wParam, lParam);
-      }
+    case IDM_EXIT: {
+      DestroyWindow(hWnd);
       break;
     }
 
-    case WM_LBUTTONDOWN: {
+    case IDC_TOGGLE_BUTTON: {
+      const wchar_t* newLabel;
+      KillTimer(hWnd, IDT_AUTO_RESIZE_TIMER);
       if (isAutoResizing) {
-        autoResizingDirection *= -1;
+        newLabel = L"Start";
+        isAutoResizing = FALSE;
       } else {
-        sizeFactor = min(sizeFactor + .1, 2.);
-        invalidateDrawArea(hWnd);
+        newLabel = L"Stop";
+        SetTimer(hWnd, IDT_AUTO_RESIZE_TIMER, 100, nullptr);
+        isAutoResizing = TRUE;
       }
+      SetWindowTextW(GetDlgItem(hWnd, IDC_TOGGLE_BUTTON), newLabel);
       break;
     }
 
-    case WM_RBUTTONDOWN: {
-      if (isAutoResizing) {
-        autoResizingDirection *= -1;
-      } else {
-        sizeFactor = max(sizeFactor - .1, .1);
-        invalidateDrawArea(hWnd);
-      }
-      break;
-    }
-
-    case WM_SIZE: {
-      sx = LOWORD(lParam);
-      sy = HIWORD(lParam);
-      break;
-    }
-
-    case WM_TIMER: {
-      if (autoResizingDirection == 1) {
-        sizeFactor = min(sizeFactor + .1, 2.);
-      } else if (autoResizingDirection == -1) {
-        sizeFactor = max(sizeFactor - .1, .1);
-      }
-      if (sizeFactor >= 2. || sizeFactor <= .1) {
-        autoResizingDirection *= -1;
-      }
-      invalidateDrawArea(hWnd);
-      break;
-    }
-
-    case WM_PAINT: {
-      PAINTSTRUCT ps;
-      const HDC hdc = BeginPaint(hWnd, &ps);
-
-      const auto hOldBrush = SelectObject(hdc, hSquareFillBrush);
-      const auto hOldPen = SelectObject(hdc, hSquareBorderPen);
-
-      const auto halfX = sx / 2;
-      const auto halfY = sy / 2;
-
-      const auto k = 200 * sizeFactor;
-
-      Rectangle(hdc,
-                static_cast<int>(halfX - k),
-                static_cast<int>(halfY - k),
-                static_cast<int>(halfX + k),
-                static_cast<int>(halfY + k));
-
-      SelectObject(hdc, hOldBrush);
-      SelectObject(hdc, hOldPen);
-
-      EndPaint(hWnd, &ps);
-      break;
-    }
-    case WM_DESTROY: {
-      DeleteObject(hAppBakgroundBrush);
-      DeleteObject(hSquareFillBrush);
-      DeleteObject(hSquareBorderPen);
-      PostQuitMessage(0);
-      break;
-    }
     default:
       return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    break;
+  }
+
+  case WM_LBUTTONDOWN: {
+    if (isAutoResizing) {
+      autoResizingDirection *= -1;
+    } else {
+      sizeFactor = min(sizeFactor + .1, 2.);
+      InvalidateDrawArea(hWnd);
+    }
+    break;
+  }
+
+  case WM_RBUTTONDOWN: {
+    if (isAutoResizing) {
+      autoResizingDirection *= -1;
+    } else {
+      sizeFactor = max(sizeFactor - .1, .1);
+      InvalidateDrawArea(hWnd);
+    }
+    break;
+  }
+
+  case WM_SIZE: {
+    sx = LOWORD(lParam);
+    sy = HIWORD(lParam);
+    break;
+  }
+
+  case WM_TIMER: {
+    if (autoResizingDirection == 1) {
+      sizeFactor = min(sizeFactor + .1, 2.);
+    } else if (autoResizingDirection == -1) {
+      sizeFactor = max(sizeFactor - .1, .1);
+    }
+    if (sizeFactor >= 2. || sizeFactor <= .1) {
+      autoResizingDirection *= -1;
+    }
+    InvalidateDrawArea(hWnd);
+    break;
+  }
+
+  case WM_PAINT: {
+    PAINTSTRUCT ps;
+    const HDC hdc = BeginPaint(hWnd, &ps);
+
+    const auto hOldBrush = SelectObject(hdc, hSquareFillBrush);
+    const auto hOldPen = SelectObject(hdc, hSquareBorderPen);
+
+    const auto halfX = sx / 2;
+    const auto halfY = sy / 2;
+
+    const auto k = 200 * sizeFactor;
+
+    Rectangle(
+        hdc,
+        static_cast<int>(halfX - k),
+        static_cast<int>(halfY - k),
+        static_cast<int>(halfX + k),
+        static_cast<int>(halfY + k));
+
+    SelectObject(hdc, hOldBrush);
+    SelectObject(hdc, hOldPen);
+
+    EndPaint(hWnd, &ps);
+    break;
+  }
+  case WM_DESTROY: {
+    DeleteObject(hAppBackgroundBrush);
+    DeleteObject(hSquareFillBrush);
+    DeleteObject(hSquareBorderPen);
+    PostQuitMessage(0);
+    break;
+  }
+  default:
+    return DefWindowProc(hWnd, message, wParam, lParam);
   }
   return 0;
 }
 
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+INT_PTR CALLBACK About(const HWND hDlg, const UINT message, const WPARAM wParam, const LPARAM lParam) {
   UNREFERENCED_PARAMETER(lParam);
   switch (message) {
-    case WM_INITDIALOG:
-      return TRUE;
-
-    case WM_COMMAND:
-      if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-        EndDialog(hDlg, LOWORD(wParam));
-        return TRUE;
-      }
-      break;
-    default:
-      return FALSE;
+  case WM_INITDIALOG: {
+    return TRUE;
   }
+
+  case WM_COMMAND: {
+    if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+      EndDialog(hDlg, LOWORD(wParam));
+      return TRUE;
+    }
+    break;
+  }
+
+  default: ;
+  }
+  return FALSE;
 }
