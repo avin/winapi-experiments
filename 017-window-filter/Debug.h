a624d9622db20f837b4cfebb1d868a076557b242
a.vinogradov <a.vinogradov@bpcbt.com>
2024-10-25 13:50:55 +0300

    upd

---
+++ 017-window-filter/Debug.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <windows.h>
+#include <cstdarg>
+#include <cwchar>
+
+void DebugOutputImpl(const wchar_t* format, ...) {
+  wchar_t msg[255] = {0};
+
+  va_list args;
+  va_start(args, format);
+  vswprintf(msg, sizeof(msg) / sizeof(wchar_t), format, args);
+  va_end(args);
+
+  OutputDebugStringW(msg);
+}
+
+#ifdef _DEBUG
+#define DebugOutput(format, ...) \
+DebugOutputImpl(format, __VA_ARGS__)
+#else
+#define DebugOutput(format, ...) \
+do {} while (0)  // Пустой макрос, который не делает ничего
+#endif
--- 017-window-filter/main.cpp
+++ 017-window-filter/main.cpp
@@ -1,4 +1,7 @@
 #include <windows.h>
+#include <magnification.h>
+
+#pragma comment(lib, "Magnification.lib")
 
 const wchar_t szWindowClass[] = L"MyWindowClass";
 const wchar_t szTitle[] = L"My Window";
@@ -7,115 +10,170 @@ HINSTANCE hInst;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-BOOL CaptureScreen(HDC hdcDest, RECT rcDest);
+BOOL bDragging = FALSE;
+POINT ptLast;
+
+HWND hMagWnd;
 
 int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                      LPWSTR lpCmdLine, int nCmdShow) {
+                      LPWSTR lpCmdLine, int nCmdShow)
+{
+    // Инициализируем библиотеку COM
+    CoInitialize(NULL);
+
     WNDCLASS wc = {};
-    wc.lpfnWndProc = WndProc;
-    wc.hInstance = hInstance;
+    wc.lpfnWndProc   = WndProc;
+    wc.hInstance     = hInstance;
     wc.lpszClassName = szWindowClass;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = NULL; // Не используем фон
 
     RegisterClass(&wc);
 
+    // Создаем основное окно
     HWND hWnd = CreateWindowEx(
-        WS_EX_TOPMOST,
+        WS_EX_TOPMOST | WS_EX_LAYERED,
         szWindowClass,
         szTitle,
-        WS_POPUP, // Без заголовочной полосы
+        WS_POPUP, // Без заголовка и границ
         CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         NULL,
         NULL,
         hInstance,
         NULL
-        );
+    );
 
-    if (!hWnd) {
+    if (!hWnd)
+    {
+        CoUninitialize();
         return FALSE;
     }
 
     hInst = hInstance;
 
+    // Инициализируем Magnification API
+    if (!MagInitialize())
+    {
+        MessageBox(NULL, L"Не удалось инициализировать Magnification API", L"Ошибка", MB_ICONERROR);
+        CoUninitialize();
+        return FALSE;
+    }
+
+    // Создаем дочернее окно увеличения
+    hMagWnd = CreateWindow(WC_MAGNIFIER, L"MagnifierWindow",
+        WS_CHILD | MS_SHOWMAGNIFIEDCURSOR,
+        0, 0, 800, 600, hWnd, NULL, hInstance, NULL);
+
+    if (!hMagWnd)
+    {
+        MessageBox(NULL, L"Не удалось создать окно увеличения", L"Ошибка", MB_ICONERROR);
+        MagUninitialize();
+        CoUninitialize();
+        return FALSE;
+    }
+
+    // Устанавливаем коэффициент увеличения 1.0 (без увеличения)
+    MAGTRANSFORM magTransform = {0};
+    magTransform.v[0][0] = 1.0f;
+    magTransform.v[1][1] = 1.0f;
+    magTransform.v[2][2] = 1.0f;
+    MagSetWindowTransform(hMagWnd, &magTransform);
+
+    // Устанавливаем фильтр для исключения нашего окна из захвата
+    MagSetWindowFilterList(hMagWnd, MW_FILTERMODE_EXCLUDE, 1, &hWnd);
+
+    // Показываем окна
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
+    ShowWindow(hMagWnd, SW_SHOW);
+    UpdateWindow(hMagWnd);
+
+    // Устанавливаем таймер для обновления области источника
+    SetTimer(hWnd, 1, 16, NULL); // Обновление ~60 FPS
+
     MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-    return (int)msg.wParam;
-}
-
-BOOL CaptureScreen(HDC hdcDest, RECT rcDest) {
-    HDC hdcScreen = GetDC(NULL); // DC всего экрана
-
-    // Создаем регион, исключающий наше окно
-    HRGN hRgnScreen = CreateRectRgn(0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN));
-    HRGN hRgnWindow = CreateRectRgn(rcDest.left, rcDest.top, rcDest.right, rcDest.bottom);
-    CombineRgn(hRgnScreen, hRgnScreen, hRgnWindow, RGN_DIFF);
-
-    SelectClipRgn(hdcScreen, hRgnScreen);
-
-    BOOL bRet = BitBlt(hdcDest, 0, 0, rcDest.right - rcDest.left, rcDest.bottom - rcDest.top,
-                       hdcScreen, rcDest.left, rcDest.top, SRCCOPY);
 
     // Освобождаем ресурсы
-    SelectClipRgn(hdcScreen, NULL);
-    DeleteObject(hRgnScreen);
-    DeleteObject(hRgnWindow);
-    ReleaseDC(NULL, hdcScreen);
+    MagUninitialize();
+    CoUninitialize();
 
-    return bRet;
+    return (int) msg.wParam;
 }
 
-BOOL bDragging = FALSE;
-POINT ptLast;
+void UpdateMagWindow(HWND hWnd)
+{
+    RECT rcClient;
+    GetClientRect(hWnd, &rcClient);
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    // Получаем размер окна
+    int width = rcClient.right - rcClient.left;
+    int height = rcClient.bottom - rcClient.top;
+
+    // Получаем положение окна на экране
+    POINT pt;
+    pt.x = rcClient.left;
+    pt.y = rcClient.top;
+    ClientToScreen(hWnd, &pt);
+
+    // Определяем область источника для захвата
+    RECT sourceRect;
+    sourceRect.left = pt.x;
+    sourceRect.top = pt.y;
+    sourceRect.right = pt.x + width;
+    sourceRect.bottom = pt.y + height;
+
+    // Устанавливаем область источника для окна увеличения
+    MagSetWindowSource(hMagWnd, sourceRect);
+
+    // Обновляем окно увеличения
+    InvalidateRect(hMagWnd, NULL, TRUE);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
     static HMENU hMenu;
 
-    switch (message) {
-        case WM_CREATE: {
-            // Создаем меню
+    switch (message)
+    {
+    case WM_CREATE:
+        {
+            // Создаем контекстное меню
             hMenu = CreatePopupMenu();
             AppendMenu(hMenu, MF_STRING, 1, L"Выйти");
-
-            // Устанавливаем таймер для регулярного обновления окна
-            SetTimer(hWnd, 1, 30, NULL); // Обновление каждые 30 мс (~33 кадра в секунду)
         }
         break;
-        case WM_TIMER: {
-            // Перерисовываем окно
-            InvalidateRect(hWnd, NULL, FALSE);
+    case WM_TIMER:
+        {
+            // Обновляем окно увеличения
+            UpdateMagWindow(hWnd);
         }
         break;
-        case WM_PAINT: {
-            PAINTSTRUCT ps;
-            RECT rc;
-            GetClientRect(hWnd, &rc);
-            HDC hdc = BeginPaint(hWnd, &ps);
-
-            POINT ptWindow = {0, 0};
-            ClientToScreen(hWnd, &ptWindow);
-            RECT rcWindow = {ptWindow.x, ptWindow.y, ptWindow.x + rc.right, ptWindow.y + rc.bottom};
-
-            CaptureScreen(hdc, rcWindow);
-
-            EndPaint(hWnd, &ps);
+    case WM_SIZE:
+        {
+            // Изменяем размер окна увеличения при изменении размера основного окна
+            RECT rcClient;
+            GetClientRect(hWnd, &rcClient);
+            MoveWindow(hMagWnd, 0, 0, rcClient.right - rcClient.left, rcClient.bottom - rcClient.top, TRUE);
         }
         break;
-        case WM_LBUTTONDOWN: {
+    case WM_LBUTTONDOWN:
+        {
             bDragging = TRUE;
             SetCapture(hWnd);
             ptLast.x = LOWORD(lParam);
             ptLast.y = HIWORD(lParam);
         }
         break;
-        case WM_MOUSEMOVE: {
-            if (bDragging) {
+    case WM_MOUSEMOVE:
+        {
+            if (bDragging)
+            {
                 POINT pt;
                 pt.x = LOWORD(lParam);
                 pt.y = HIWORD(lParam);
@@ -127,16 +185,18 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
                 int dy = pt.y - ptLast.y;
 
                 MoveWindow(hWnd, rcWindow.left + dx, rcWindow.top + dy,
-                           rcWindow.right - rcWindow.left, rcWindow.bottom - rcWindow.top, TRUE);
+                    rcWindow.right - rcWindow.left, rcWindow.bottom - rcWindow.top, TRUE);
             }
         }
         break;
-        case WM_LBUTTONUP: {
+    case WM_LBUTTONUP:
+        {
             bDragging = FALSE;
             ReleaseCapture();
         }
         break;
-        case WM_RBUTTONUP: {
+    case WM_RBUTTONUP:
+        {
             POINT pt;
             pt.x = LOWORD(lParam);
             pt.y = HIWORD(lParam);
@@ -145,20 +205,23 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) 
             TrackPopupMenu(hMenu, TPM_RIGHTBUTTON, pt.x, pt.y, 0, hWnd, NULL);
         }
         break;
-        case WM_COMMAND: {
+    case WM_COMMAND:
+        {
             if (LOWORD(wParam) == 1) // "Выйти"
             {
                 PostQuitMessage(0);
             }
         }
         break;
-        case WM_DESTROY:
-            KillTimer(hWnd, 1); // Останавливаем таймер
+    case WM_DESTROY:
+        {
+            KillTimer(hWnd, 1);
             DestroyMenu(hMenu);
             PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
 }
